c6d6396248171ae9305e1a93cbd2f9117acc2f2e
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-27 15:55:34 +0100

    feat(excercise-3): done

--- week-02/day-01/Excercise-3/Excercise-3/Program.cs
+++ week-02/day-01/Excercise-3/Excercise-3/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace Excercise_3
+namespace GreenFox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Esther!");
+            Console.WriteLine("Hello, Mary!");
+            Console.WriteLine("Hello, Joe!");
+            Console.ReadLine();
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
         }
     }
 }
