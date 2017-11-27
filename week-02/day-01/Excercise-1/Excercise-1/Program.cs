9599bc2b5dfb06fc8132d9b5958a5398034bc564
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-27 15:29:03 +0100

    feat(excercise-1):done

---
+++ week-02/day-01/Excercise-1/Excercise-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nóra!");
+            Console.ReadLine();
+        }
+    }
+}
