0d4382b05949764cc3d047e6663cbacf59a904bc
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-27 16:58:30 +0100

    feat(excercise-5): done

---
+++ week-02/day-01/Excercise-5/Excercise-5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 22;
+            int b = 13;
+            double c;
+
+            c = a + b;
+            Console.WriteLine("22+13={0}", c);
+
+            c = a - b;
+            Console.WriteLine("22-13={0}", c);
+
+            c = a * b;
+            Console.WriteLine("22*13={0}", c);
+
+            c = 22.0 / 13.0;
+            Console.WriteLine("22/13={0}", c);
+
+            c = a / b;
+            Console.WriteLine("22/13={0}", c);
+
+            c = a % b;
+            Console.WriteLine("22/13's reminder={0}", c);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
