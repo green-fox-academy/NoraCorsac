a042c27a77f3fe60118f2a3acc6f5995874ade64
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-27 15:59:52 +0100

    feat(excercise-4): done

---
+++ week-02/day-01/Excercise-4/Excercise-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nóra Fraknóy");
+            Console.WriteLine("27");
+            Console.WriteLine("1,70");
+            Console.ReadLine();
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+        }
+    }
+}
