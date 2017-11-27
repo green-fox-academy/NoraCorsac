9697cf64f856ab546c6a5258034da19416df7b29
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-27 17:38:42 +0100

    feat(excercise-6): half done

---
+++ week-02/day-01/Excercise-6/Excercise-6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 17;
+            int c = 5;
+            int d;
+            double e;
+
+            d = a * b * c;
+            Console.WriteLine("An average Green Fox attendee is spent with coding in a semester {0} hours, if the attendee only codes on workdays.", d);
+
+            e = 30 / 52 * 100;
+            Console.WriteLine("The percentage of coding hours in the semester is {0} %, if the average work hurs weekly is 52.", e);
+
+            Console.ReadLine();
+          
+         
+        }
+    }
+}
