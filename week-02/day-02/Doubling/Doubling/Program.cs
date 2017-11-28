6328b4afc136d8ef0e0a7289dfcb40c9c1cedb50
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-28 12:04:04 +0100

    feat(excercise1): done

---
+++ week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(doubling(ak));
+
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+            Console.ReadLine();
+        }
+        public static int doubling(int num1)
+        {
+            return (num1 * 2);
+        }
+    }
+}
---
+++ week-02/day-02/Greet/Greet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            
+            Console.WriteLine("Greetings dear, " + al + "!");
+
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            Console.ReadLine();
+        }
+    }
+}
