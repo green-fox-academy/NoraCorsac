0999cd02aae49a9e6a7998acd908723255ed0525
Nora Fraknoy <norafraknoy@gmail.com>
2017-11-28 13:26:03 +0100

    feat(AppendA): done

---
+++ week-02/day-02/AppendA/AppendA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            AppendA(am);
+
+
+            
+
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+            Console.ReadLine();
+        }
+        public static void AppendA(string word)
+        {
+            Console.WriteLine(word + "a");
+        }
+    }
+}
