314a762d28b3a94b0a8d5c275899dbc31432c1df
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-01 04:35:35 +0100

    feat(excercise-16): done

--- week-02/day-01/Excercise-15/Excercise-15/Program.cs
+++ week-02/day-01/Excercise-15/Excercise-15/Program.cs
@@ -6,15 +6,20 @@ namespace GreenFox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please add the km:");
-            string add = Console.ReadLine();
-            int number = int.Parse(add);
-            double miles = number / 1.621371192;
+            Console.WriteLine("Please add the number of chickens has the farmer: ");
+            string chicken = Console.ReadLine();
+            int chickenLegs = int.Parse(chicken) * 2;
+            Console.WriteLine("Please add the number of pigs the farmer has: ");
+            string pig = Console.ReadLine();
+            int pigLegs = int.Parse(pig) * 4;
+            int allLegs = chickenLegs + pigLegs;
 
-            Console.WriteLine("Thats {0} miles.", miles);
+            Console.WriteLine("The animals have {0} legs.", allLegs);
             Console.ReadLine();
-            // Write a program that asks for an integer that is a distance in kilometers,
-            // then it converts that value to miles and prints it
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
         }
     }
 }
