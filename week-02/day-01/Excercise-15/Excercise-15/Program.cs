e0137428c3e2d119e2a2993095facce98798d71b
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-01 04:22:05 +0100

    feat(excercise-15):done

---
+++ week-02/day-01/Excercise-15/Excercise-15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please add the km:");
+            string add = Console.ReadLine();
+            int number = int.Parse(add);
+            double miles = number / 1.621371192;
+
+            Console.WriteLine("Thats {0} miles.", miles);
+            Console.ReadLine();
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+        }
+    }
+}
