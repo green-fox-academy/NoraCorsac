511a7582f3bdb0cfaef4d0cbdb0a3cc5164489b5
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-01 04:08:52 +0100

    feat(excercise-13-14): done

---
+++ week-02/day-01/Excercise-13/Excercise-13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+            int allHours = 24;
+            int allMinutes = 60;
+            int allSeconds = 60;
+
+            int secondsADay = allHours * allMinutes * allSeconds;
+            int secondsCurrent = currentHours * currentMinutes * currentSeconds;
+
+            Console.WriteLine(secondsADay - secondsCurrent + " seconds remaining.");
+            Console.ReadLine();
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
---
+++ week-02/day-01/eXCERCISE-14/eXCERCISE-14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+            Console.ReadLine();
+
+        }
+    }
+}
