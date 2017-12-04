58ca44b0509ac52b95eeedd13abbb4a23691d472
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-04 12:09:32 +0100

    feat(DivideByZero): done

---
+++ week-03/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a number: ");
+
+            try
+            {
+                int divisor = int.Parse(Console.ReadLine());
+                int result = 10 / divisor;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("fail");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
