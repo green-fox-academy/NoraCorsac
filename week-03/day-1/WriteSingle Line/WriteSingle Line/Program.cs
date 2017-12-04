ba7685cb3f9693c207d0b8344f66c00945dae3b4
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-04 16:12:26 +0100

    feat(WriteSingleLine): done

--- week-03/day-1/WriteSingle Line/WriteSingle Line/Program.cs
+++ week-03/day-1/WriteSingle Line/WriteSingle Line/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WriteSingle_Line
 {
@@ -6,7 +7,19 @@ namespace WriteSingle_Line
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                File.WriteAllText("my-file.txt", "Nóra Fraknóy");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.ReadLine();
+            }
             
         }
     }
