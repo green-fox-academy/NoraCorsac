2018058402ee0db984289bc8adc99c3d49771c27
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-04 12:30:52 +0100

    feat(PrintEachLine): done

---
+++ week-03/day-1/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                File.ReadAllText(@"my-file.txt");
+                Console.WriteLine(File.ReadAllText(@"my-file.txt"));
+                Console.ReadKey();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Uh-oh, could not read the file!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
