c025b2cc24e2bbbc93e8f3fd4a00ec66ab15a7ce
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-06 12:12:15 +0100

    feat(Diagonals): make diagonals

---
+++ week-03/day-3/Diagonals/Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Diagonals
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Diagonals(foxDraw);
+            // draw the canvas' diagonals in green.
+
+        }
+        public static void Diagonals(FoxDraw foxDraw)
+        {
+            {
+                foxDraw.StrokeColor(Colors.Pink);
+                foxDraw.DrawLine(0, 0, 525, 350);
+
+                foxDraw.StrokeColor(Colors.LimeGreen);
+                foxDraw.DrawLine(525, 0, 0, 350);
+            }
+        }
+    }
+}
