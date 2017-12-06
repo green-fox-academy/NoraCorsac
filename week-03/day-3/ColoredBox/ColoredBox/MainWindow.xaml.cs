5c0346185a812cced6e946829d22cd50f0158ca0
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-06 12:00:45 +0100

    feat(ColoredBox): make a box with colored lines

---
+++ week-03/day-3/ColoredBox/ColoredBox/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace ColoredBox
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            ColoredBox(foxDraw);
+            // draw a box that has different colored lines on each edge.
+
+        }
+
+        public static void ColoredBox(FoxDraw foxDraw)
+        {
+            {
+                foxDraw.StrokeColor(Colors.Pink);
+                foxDraw.DrawLine(0, 0, 100, 0);
+
+                foxDraw.StrokeColor(Colors.LimeGreen);
+                foxDraw.DrawLine(100, 0, 100, 100);
+
+                foxDraw.StrokeColor(Colors.CornflowerBlue);
+                foxDraw.DrawLine(100, 100, 0, 100);
+
+                foxDraw.StrokeColor(Colors.LightGoldenrodYellow);
+                foxDraw.DrawLine(0, 100, 0, 0);
+            }
+        }
+    }
+}
