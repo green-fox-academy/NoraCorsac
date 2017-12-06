3b9968a9d2fd3fa8a61898792c5de5bf01de2bf0
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-06 15:02:53 +0100

    feat(CenteredSquare): make square

---
+++ week-03/day-3/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace CenteredSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a green 10x10 square to the canvas' center.
+            Horizontal(foxDraw);
+
+        }
+        public static void Horizontal(FoxDraw foxDraw)
+        {
+            {
+                foxDraw.StrokeColor(Colors.LightSeaGreen);
+                foxDraw.DrawRectangle(255, 165, 10, 10);
+            }
+        }
+    }
+}
