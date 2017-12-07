63ace97de42585e77165ffc67366f09bd357ff23
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-08 00:07:47 +0100

    feat(Power): recursively works

---
+++ week-03/day-4/Power/Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumber = 5;
+            int exponent = 3;
+            Console.WriteLine(Power(baseNumber, exponent));
+            Console.ReadLine();
+        }
+        static int Power(int baseNumber, int exponent)
+        {
+            if (exponent == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseNumber * Power(baseNumber, exponent - 1);
+            }
+
+        }
+    }
+}
