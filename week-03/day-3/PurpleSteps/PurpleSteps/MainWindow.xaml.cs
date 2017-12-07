82592558432610eeaf859d101c0287fbde745235
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-07 14:54:17 +0100

    feat(PurpleSteps):add purple boxes

---
+++ week-03/day-3/PurpleSteps/PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+            PurpleSteps(foxDraw);
+        }
+        public static void PurpleSteps(FoxDraw foxDraw)
+        {
+            {
+                int x = 10;
+                
+                Random randomColor = new Random();
+                for (int i = 0; i < 19; i++)
+                {
+                    foxDraw.FillColor(Colors.MediumPurple);
+                    foxDraw.DrawRectangle(x * i, x * i, x, x);
+                }
+            }
+        }
+    }
+}
