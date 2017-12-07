9ccbf84808c6771df73325ddddfdfbc145a7b257
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-07 11:43:06 +0100

    feat(1-Counter): add recursion

---
+++ week-03/day-4/1-Counter/1-Counter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter(15);
+            Console.ReadLine();
+            
+        }
+        static int Counter (int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Counter(n - 1);
+            }
+
+        }
+    }
+}
