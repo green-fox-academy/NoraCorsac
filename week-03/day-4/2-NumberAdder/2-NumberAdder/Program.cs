46745afadbb801b8694d8440d0dd6ef74149951d
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-07 13:21:34 +0100

    feat(2-NumberAdder): add number

---
+++ week-03/day-4/2-NumberAdder/2-NumberAdder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(5)); 
+            Console.ReadLine();
+
+        }
+        static int Counter(int number)
+        {
+            if (number ==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number + Counter(number - 1);
+            }
+        }
+    }
+}
