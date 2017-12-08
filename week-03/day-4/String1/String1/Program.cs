000c8f8c53ec710204a1607fcfeb2127bf140953
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-08 05:38:56 +0100

    feat(String1): replace x to y

---
+++ week-03/day-4/String1/String1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(String1(input));
+            Console.ReadLine();
+        }
+        static string String1(string input)
+        {
+            if (input.IndexOf("x") == -1)
+            {
+                return input;
+            }
+            else
+            {
+                int index = input.IndexOf("x");
+                input = input.Remove(index, 1);
+                input = input.Insert(index, "y");
+                return String1(input);
+            }
+
+        }
+
+    }
+}
