7f4f35c47112d154ec3228f3c929b22487bb1eaf
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-08 05:16:31 +0100

    feat(Bunny2):ears counted well

---
+++ week-03/day-4/Bunny2/Bunny2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bunny2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ears(120));
+            Console.ReadLine();
+
+        }
+        static int Ears(int bunny)
+        {
+            if (bunny == 0)
+            {
+                return 0;
+            }
+            else if (bunny % 2 == 0)
+            {
+                return 2 + Ears(bunny - 1);
+            }
+            else
+            {
+                return 3 + Ears(bunny - 1);
+            }
+        }
+    }
+}
