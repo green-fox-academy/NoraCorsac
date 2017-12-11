47beb2d62ff879345db0722f60027ce9ad796cab
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-11 13:57:53 +0100

    feat(Post-it): 3 postIt added

---
+++ week-04/day-1/Post-it/Post-it/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Post_it;
+
+namespace Post_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PostIt1 = new PostIt();
+
+            PostIt1.TextColor = "blue";
+            PostIt1.BackgroundColor = "orange";
+            PostIt1.Text = "Idea 1";
+
+            var PostIt2 = new PostIt();
+
+            PostIt2.TextColor = "black";
+            PostIt2.BackgroundColor = "pink";
+            PostIt2.Text = "Awesome";
+
+            var PostIt3 = new PostIt();
+
+            PostIt3.TextColor = "green";
+            PostIt3.BackgroundColor = "Yellow";
+            PostIt3.Text = "Superb!";
+        }
+    }
+}
