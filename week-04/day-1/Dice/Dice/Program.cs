a991d2ddae7689641e4aa788445c37b5aa0dc2bf
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-11 15:25:36 +0100

    feat(Dice): dices added

--- week-04/day-1/Dice/Dice/Program.cs
+++ week-04/day-1/Dice/Dice/Program.cs
@@ -10,6 +10,17 @@ namespace Dice
     {
         static void Main(string[] args)
         {
+            List<Dice> dices = new List<Dice>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                dices.Add(new Dice() { D6 = 6 });
+            }
+
+            for (int j = 0; j < dices.Count; j++)
+            {
+                dices[j].Roll();  
+            }
         }
     }
 }
