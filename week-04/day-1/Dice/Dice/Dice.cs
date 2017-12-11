b0005c31d0ce56afe042bcf8bebd061e4a0527cc
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-11 14:36:11 +0100

    feat(Dice): random add

---
+++ week-04/day-1/Dice/Dice/Dice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        public int D6 { get; set; }
+
+        public void Roll()
+        {
+            Random random = new Random();
+            D6 = random.Next(1, 7);
+        }
+    }
+}
