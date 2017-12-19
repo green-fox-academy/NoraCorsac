aef2f7e485454a0bc499b551e15b349e4f49c834
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-19 19:38:58 +0100

    feat(Excercise-25): add for loop

---
+++ week-02/day-01/Excercise-25/Excercise-25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the multiplicand number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", i, number, i * number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
