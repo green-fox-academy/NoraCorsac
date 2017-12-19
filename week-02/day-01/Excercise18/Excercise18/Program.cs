24183bf7ef9b73bbe2068648c79cb5e5d55743df
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-19 17:15:26 +0100

    feat(Excercise-18): add if statement

---
+++ week-02/day-01/Excercise18/Excercise18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (number % 2 != 0)
+            {
+                Console.WriteLine("Your number is Odd");
+            }
+            else
+            {
+                Console.WriteLine("Your number is Even");
+            }
+
+        }    
+    }
+}
