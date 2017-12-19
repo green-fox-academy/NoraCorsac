9c6581aeb801695844e79144fb4e65d9c9fb623b
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-19 19:26:51 +0100

    feat(Excercise-23): add for loop

---
+++ week-02/day-01/Excercise-24/Excercise-24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints all the even numbers between 0 and 500
+            for (int i = 0; i < 500; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
