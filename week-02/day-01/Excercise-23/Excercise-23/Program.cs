da3f8977317173b33ff09fbb6c754890f9630dc6
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-19 19:24:00 +0100

    feat(Excercise-23): add for loop

---
+++ week-02/day-01/Excercise-23/Excercise-23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "I won't cheat on the exam!";
+
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+             for (int i = 0; i < 100; i++)
+             {
+                 Console.WriteLine(a);
+             }
+                 Console.ReadLine();
+        }
+    }
+}
