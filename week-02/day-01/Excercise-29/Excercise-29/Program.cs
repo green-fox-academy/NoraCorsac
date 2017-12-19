bba8582c738c47203d442766fadebe15d295bfcf
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-19 21:48:17 +0100

    feat(Excercise-29):add for loops

---
+++ week-02/day-01/Excercise-29/Excercise-29/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please add the number of triangle's lines: ");
+            string input = Console.ReadLine();
+            int lines = int.Parse(input);
+
+            for (int i = 0; i <= lines; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
