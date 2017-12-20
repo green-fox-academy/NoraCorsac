ed685ac77b1084e1f7f843945b0a8e3ab6302356
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-20 20:42:53 +0100

    feat(FleetOfThimgs): half done

--- week-04/day-1/BlogPost/BlogPost/BlogPost.cs
+++ week-04/day-1/BlogPost/BlogPost/BlogPost.cs
@@ -8,5 +8,9 @@ namespace BlogPost
 {
     class BlogPost
     {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
     }
 }
---
+++ week-04/day-1/Counter/Counter/Counter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int MyValue { get; private set; } = 10;
+    }
+
+    public void Add(int number)
+    {
+        MyValue += number;
+    }
+}
