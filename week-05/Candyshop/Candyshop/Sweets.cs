34e541b2e6a3eea75e4b0ba1c5a7bb0028238c61
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-21 13:50:27 +0100

    feat(CandyShop): add classes

---
+++ week-05/Candyshop/Candyshop/Sweets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candyshop
+{
+    public class Sweets
+    {
+        public double Sugar { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+    }
+}
