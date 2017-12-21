c64d40da7045e85095fe51d919bfcc1b69a886c1
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-21 20:55:01 +0100

    feat(Garden): pff

--- week-05/Garden/Garden/Garden.cs
+++ week-05/Garden/Garden/Garden.cs
@@ -8,5 +8,24 @@ namespace Garden
 {
     class Garden
     {
+        public string Color { get; set; }
+        public int Water { get; set; } = 0;
+        public double AbsorbCapacity { get; set; }
+
+        List<Flower> flowers = new List<Flower>();
+        List<Tree> trees = new List<Tree>();
+
+        public Garden(string color, int water, double absorbcapacity)
+        {
+            Color = color;
+            Water = water;
+            AbsorbCapacity = absorbcapacity;
+        }
+
+        public void PlantList(Plant plant)
+        {
+            List<Plant> PlantList = new List<Plant>();
+
+        }
     }
 }
