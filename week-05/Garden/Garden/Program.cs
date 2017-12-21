9e7dbc95061b43d9c4b9dd09cf5c4e02dbc78a74
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-21 17:18:22 +0100

    feat(Garden): add datas

---
+++ week-05/Garden/Garden/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gardenList = new List<Garden>();
+            var tree = new Tree();
+            var flower = new Flower();
+            gardenList.Add(tree);
+            gardenList.Add(flower);
+        }
+    }
+}
