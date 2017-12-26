c4104ebc3b55b5f7aef8153aabd1cb20d1616213
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 12:45:21 +0100

    feat(IntroduceMyself): add datas

---
+++ after-exam-practice/cw/IntroduceMyself/IntroduceMyself/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroduceMyself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nóri Fraknóy";
+            int age = 27;
+            double height = 1.70;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.ReadLine();
+        }
+    }
+}
