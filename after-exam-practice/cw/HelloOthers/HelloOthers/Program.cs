7c3e6535e3364ca5709cd0366aa5ee93ce9382a8
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 12:42:06 +0100

    feat(HelloOthers): say more hello

---
+++ after-exam-practice/cw/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Esther!");
+            Console.WriteLine("Hello Mary!");
+            Console.WriteLine("Hello Joe!");
+            Console.ReadLine();
+        }
+    }
+}
