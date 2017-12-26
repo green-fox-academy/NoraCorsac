431c3c3a35c054a79fbd6bd98795b704b04c0e1f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 13:38:57 +0100

    feat(DefineInfos): add datas

---
+++ after-exam-practice/cw/DefineInfos/DefineInfos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DefineInfos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nóri Fraknóy";
+            int age = 27;
+            double height = 1.70;
+            bool marriage = true;
+            Console.WriteLine("Hello! My name is {0}, I'm {1} years old and {2} meters high. Am I married? {3}!", name, age, height, marriage);
+            Console.ReadLine();
+        }
+    }
+}
