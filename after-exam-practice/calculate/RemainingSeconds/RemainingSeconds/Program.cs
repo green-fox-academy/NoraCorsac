af18d9ca3faf9d1b709c33bb22e1b9d2fc7e6e60
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 14:15:47 +0100

    feat(RemainingSeconds): done

---
+++ after-exam-practice/calculate/RemainingSeconds/RemainingSeconds/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RemainingSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            int secondsInADay = 86400;
+            int changeNumber = 60;
+
+            int remainingSeconds = secondsInADay - (currentHours * changeNumber * changeNumber + currentMinutes * changeNumber + currentSeconds);
+            Console.WriteLine("{0} seconds remaining.", remainingSeconds);
+            Console.ReadLine();
+        }
+    }
+}
