48264812792b14f42d4a2a9b6c444a45d47d0318
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 14:09:21 +0100

    feat(Cuboid): clear magic numbers

---
+++ after-exam-practice/calculate/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sides = 10;
+            double powerSurfaceArea = 2;
+            double powerVolume = 3;
+            double cuboidSides = 6;
+
+            double surfaceArea = (Math.Pow(sides, powerSurfaceArea)) * cuboidSides;
+            double volume = Math.Pow(sides, powerVolume);
+
+            Console.WriteLine("Surface Area: {0}\nVolume: {1}", surfaceArea, volume);
+            Console.ReadLine();
+        }
+    }
+}
