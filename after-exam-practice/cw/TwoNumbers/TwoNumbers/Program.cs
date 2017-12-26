5665d20056aa7bff890932f4ee565ff05a9af58f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 12:56:50 +0100

    feat(TwoNumbers): add operations

---
+++ after-exam-practice/cw/TwoNumbers/TwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 22;
+            int b = 13;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a / 13.0);
+            Console.WriteLine(a % b);
+            Console.ReadLine();
+        }
+    }
+}
