005c0d76191cb2bf997c2792aac14b6ef174387e
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 13:23:11 +0100

    feat(MyFavoriteNumber): add my number

---
+++ after-exam-practice/cw/FavoriteNumber/FavoriteNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myFavoriteNumber = 10;
+            Console.WriteLine("My favorite number is {0}.", myFavoriteNumber);
+            Console.ReadLine();
+        }
+    }
+}
