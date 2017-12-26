cf31ba63bf79b64ee39dadc5b37354a107f46ce1
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 12:34:00 +0100

    feat(HelloMe): say hello

---
+++ after-exam-practice/cw/HelloMe/HelloMe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nóri!");
+            Console.ReadLine();
+        }
+    }
+}
