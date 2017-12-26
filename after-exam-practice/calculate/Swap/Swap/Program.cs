de4cbe387903a4bfcdaac75368aa4a47cdb270af
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-26 13:28:21 +0100

    feat(Swap): change numbers

---
+++ after-exam-practice/calculate/Swap/Swap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
