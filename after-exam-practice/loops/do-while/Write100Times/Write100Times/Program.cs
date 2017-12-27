9e403609a037fa087086d9df695d4ac9c9d44f9e
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 04:00:58 +0100

    feat(Write100Times): add while loop

---
+++ after-exam-practice/loops/do-while/Write100Times/Write100Times/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Write100Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a < 100)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
