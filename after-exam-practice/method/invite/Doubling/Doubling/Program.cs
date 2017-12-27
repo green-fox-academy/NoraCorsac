d84a1301145061a294aa3349fe1a35d2cc28e678
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 21:08:04 +0100

    feat(Doubling): add return

---
+++ after-exam-practice/method/invite/Doubling/Doubling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = 123;
+            Console.WriteLine(doubling(ak));
+            Console.ReadLine();
+        }
+        public static int doubling(int number1)
+        {
+            return (number1 * 2);
+        }
+    }
+}
