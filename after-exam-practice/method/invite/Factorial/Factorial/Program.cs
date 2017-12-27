f99a8e9717baf10471a5f487c1cf28e51194a3cf
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 21:50:20 +0100

    feat(Factorial): add for loop

---
+++ after-exam-practice/method/invite/Factorial/Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please add your number: ");
+            string input = Console.ReadLine();
+            int inputNumber = int.Parse(input);
+            Console.WriteLine(Factorio(inputNumber));
+            Console.ReadLine();
+        }
+        public static int Factorio(int number)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
