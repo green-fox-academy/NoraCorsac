b178c9737a710a70610a5a67816b4751297e7364
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 02:52:12 +0100

    feat(MileToKmConverter): add parse

---
+++ after-exam-practice/calculate/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the distance in km: ");
+            string distance = Console.ReadLine();
+            int km = int.Parse(distance);
+            double mile = km * 0.621371192;
+            Console.WriteLine("The distance is {0} mile.", mile);
+            Console.ReadLine();
+        }
+    }
+}
