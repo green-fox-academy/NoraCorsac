797a1bcb4d4c0e66480a21dd4cae25bb2a160c9c
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 21:21:16 +0100

    feat(Greet): say hello

---
+++ after-exam-practice/method/invite/Greet/Greet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string al = "Greenfox";
+            greet(al);
+            Console.ReadLine();
+        }
+        public static void greet(string name)
+        {
+            Console.WriteLine("Hello dear, " + name + "!");
+        }
+    }
+}
