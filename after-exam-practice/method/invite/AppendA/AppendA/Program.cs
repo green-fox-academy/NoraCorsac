2885362d14813900c38d047bafdc659f91233f84
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 21:27:18 +0100

    feat(AppendA): add method

---
+++ after-exam-practice/method/invite/AppendA/AppendA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "Kuty";
+            appendA(am);
+        }
+        public static void appendA(string word)
+        {
+            Console.WriteLine(word + "a");
+            Console.ReadLine();
+        }
+    }
+}
