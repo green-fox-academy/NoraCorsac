af8d4a8aeed874f0f11298b54b329e41b042b29a
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 03:06:23 +0100

    feat(AnimalsAndLegs): add all legs

---
+++ after-exam-practice/calculate/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Farmer!\nHow many chickens do you have? Please enter here: ");
+            string chickensInput = Console.ReadLine();
+            int chickenLegs = int.Parse(chickensInput);
+
+            Console.WriteLine("Great! And how many pigs do you have? Please enter that here too: ");
+            string pigsInput = Console.ReadLine();
+            int pigsLegs = int.Parse(pigsInput);
+
+            int numberOfLegs = chickenLegs * 2 + pigsLegs * 4;
+            Console.WriteLine("Sounds great! So your animals have {0} legs in all.", numberOfLegs);
+            Console.ReadLine();
+        }
+    }
+}
