3fb2981325107d96cede6a5aa680cd6dbd05878c
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 10:30:22 +0100

    feat(MultiplicationTable): add for loop

---
+++ after-exam-practice/loops/for/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the multiplicand number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", i, number, i * number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
