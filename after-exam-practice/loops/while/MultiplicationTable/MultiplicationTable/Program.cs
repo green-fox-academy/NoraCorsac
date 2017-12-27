56e81030ff4674266c26f9a2de8e98f1d6750e7d
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 10:35:31 +0100

    feat(MultiplicationTable): add while loop

---
+++ after-exam-practice/loops/while/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the multiplicand number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            int multiplicationNumber = 1;
+
+            while (multiplicationNumber <= 10)
+            {
+                Console.WriteLine("{0} * {1} = {2}", multiplicationNumber, number, multiplicationNumber * number);
+                multiplicationNumber++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
