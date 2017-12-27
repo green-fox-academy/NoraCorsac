e9f36f297398244de480ab38f15a709134062d24
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 04:10:06 +0100

    feat(Write100Times): add for loop

---
+++ after-exam-practice/loops/for/Write100Times/Write100Times/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Write100Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+            Console.WriteLine("{0}. I won't cheat on the exam!", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
