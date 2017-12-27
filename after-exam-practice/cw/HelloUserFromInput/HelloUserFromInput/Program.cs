8a716296ffba2e82f91100ffc21dbec4f10402ec
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 02:40:58 +0100

    feat(HelloUserFromInput): say hello

---
+++ after-exam-practice/cw/HelloUserFromInput/HelloUserFromInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloUserFromInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What's your name? Please enter: ");
+            Console.WriteLine("Hello, {0}! Have a nice day!", Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
