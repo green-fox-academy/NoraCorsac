a33e7d5f847b5f6c86738719c2f4e79f1be10227
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 21:40:29 +0100

    feat(Summing): add return

---
+++ after-exam-practice/method/invite/Summing/Summing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please add your number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            Console.WriteLine(Sum(number));
+            Console.ReadLine();
+
+        }
+        public static int Sum(int number)
+        {
+            return (number * (number + 1) / 2);
+        }
+    }
+}
