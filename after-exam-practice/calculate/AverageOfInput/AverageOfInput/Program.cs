f71c7cd373dde4b88c0322ecef2ca03b2b14eb40
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 03:24:08 +0100

    feat(AverageOfInput): add numbers

---
+++ after-exam-practice/calculate/AverageOfInput/AverageOfInput/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AverageOfInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nIf you enter 5 integer in a row, I will tell you it's sum ant it's average.\nEnter the numbers here: ");
+
+            string firstEnteredNumber = Console.ReadLine();
+            int firstNumber = int.Parse(firstEnteredNumber);
+            string secondEnteredNumber = Console.ReadLine();
+            int secondNumber = int.Parse(secondEnteredNumber);
+            string thirdEnteredNumber = Console.ReadLine();
+            int thirdNumber = int.Parse(thirdEnteredNumber);
+            string fourthEnteredNumber = Console.ReadLine();
+            int fourthNumber = int.Parse(fourthEnteredNumber);
+            string fifthEnteredNumber = Console.ReadLine();
+            int fifthNumber = int.Parse(fifthEnteredNumber);
+
+            int sumOfNumbers = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
+            double averageOfNumbers = sumOfNumbers / 5;
+
+            Console.WriteLine("Your number's sum is: {0}, and it's average is: {1}.", sumOfNumbers , averageOfNumbers);
+            Console.ReadLine();
+
+        }
+    }
+}
