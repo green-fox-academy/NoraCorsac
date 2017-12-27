a692377eaac0795ba62556ed0991195523445a1c
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 10:43:01 +0100

    feat(CountFromTo): add if with for loop

---
+++ after-exam-practice/loops/for/CountFromTo/CountFromTo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CountFromTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string input1 = Console.ReadLine();
+            int number1 = int.Parse(input1);
+
+            Console.WriteLine("Please enter a second number: ");
+            string input2 = Console.ReadLine();
+            int number2 = int.Parse(input2);
+
+            if (number1 >= number2)
+            {
+                Console.WriteLine("The second number should be bigger!");
+            }
+            else
+            {
+                for (int i = 0; i < (number2 - number1); i++)
+                {
+                    Console.WriteLine(i + number1);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
