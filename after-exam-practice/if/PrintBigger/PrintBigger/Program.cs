bec8ec16fa3334b4db357b5129d57f592ab280af
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 03:45:29 +0100

    feat(PrintBigger): add if statements

---
+++ after-exam-practice/if/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nPlease enter two integer in a row, than I will tell you, which is the bigger!\nYour numbers: ");
+
+            string firstInputNumber = Console.ReadLine();
+            int firstNumber = int.Parse(firstInputNumber);
+            string secondInputNumber = Console.ReadLine();
+            int secondNumber = int.Parse(secondInputNumber);
+
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("The numbers are equal!");
+            }
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine("{0} is smaller than {1}!", firstNumber, secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("{0} is bigger than {1}!", firstNumber, secondNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
