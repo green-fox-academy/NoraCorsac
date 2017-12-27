5fa4cff26033bdbd75da309d1733c34984aeab72
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 03:37:33 +0100

    feat(OneTwoALot): finish

---
+++ after-exam-practice/if/OneTwoALot/OneTwoALot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OneTwoALot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nPlease write here your number: ");
+            string inputNumber = Console.ReadLine();
+            int userNumber = int.Parse(inputNumber);
+
+            if (userNumber <= 0)
+            {
+                Console.WriteLine("Not enough!!");
+            }
+            else if (userNumber == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (userNumber == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("A lot");
+            }
+            Console.ReadLine();
+        }
+    }
+}
