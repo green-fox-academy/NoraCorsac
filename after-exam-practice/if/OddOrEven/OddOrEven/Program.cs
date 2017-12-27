5d16dc0964399375e0dfad60884f278a0fe6b4c0
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 03:32:43 +0100

    feat(OddOrEven): finish

---
+++ after-exam-practice/if/OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nPlease enter a number, and I will tell you, this number is odd or even!\nThe number: ");
+            string usersNumber = Console.ReadLine();
+            int userNumber = int.Parse(usersNumber);
+
+            if (userNumber % 2 == 0)
+            {
+                Console.WriteLine("Hurray! Your number is even!");
+            }
+            else
+            {
+                Console.WriteLine("Hurray! Your number is odd!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
