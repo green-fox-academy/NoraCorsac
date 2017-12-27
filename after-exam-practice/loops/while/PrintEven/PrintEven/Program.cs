dbaea0aebafaec93dc6ec0ab2a48dbd3b776132f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 10:09:29 +0100

    feat(PrintEven): add while loop

---
+++ after-exam-practice/loops/while/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a <= 500)
+            {
+            Console.WriteLine(a++);
+            }
+            Console.ReadLine();
+        }
+    }
+}
