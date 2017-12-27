410206bf5c29436262ff0f7cacd2f6d547df1289
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-27 10:25:42 +0100

    feat(PrintEven): add do-while loop with if

---
+++ after-exam-practice/loops/do-while/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -2;
+            do
+            {
+                if (a % 2 == 0)
+                {
+                Console.WriteLine(a += 2);
+                }
+            }
+            while (a < 500);
+            Console.ReadLine();
+        }
+    }
+}
