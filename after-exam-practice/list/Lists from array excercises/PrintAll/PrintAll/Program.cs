d0b4172d54a38787d3fb4c7195259df424707b70
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 23:06:43 +0100

    feat(PrintAll): convert array to list

---
+++ after-exam-practice/list/Lists from array excercises/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            List<int> afList = new List<int>();
+            afList = af.ToList();
+
+            for (int i = 0; i < afList.Count; i++)
+            {
+                Console.WriteLine(afList[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
