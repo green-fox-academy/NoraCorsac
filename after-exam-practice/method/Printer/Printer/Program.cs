827ecfc4f35892241f66f132aebcde046156914e
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 06:00:06 +0100

    feat(Printer): add for loop

---
+++ after-exam-practice/method/Printer/Printer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer("first", "second", "third", "fourth");
+            Console.ReadLine();
+        }
+        public static void Printer(params string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+    }
+}
