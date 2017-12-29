10f5691474a55013677a8ef7e63c0a0b0b3ec504
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 06:24:33 +0100

    feat(ChangeElement): change element

---
+++ after-exam-practice/array/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `s`
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            //   with the following content: `[1, 2, 3, 8, 5, 6]`
+            s[3] = 4;
+            // - Change the 8 to 4
+            // - Print the fourth element
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+        }
+    }
+}
