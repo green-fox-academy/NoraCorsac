1f6579742cbd98f994f7dca6a2d51a507956c524
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 16:52:21 +0100

    feat(AppendLetter): add more solutions

---
+++ after-exam-practice/list/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+            /*for (int i = 0; i < far.Count; i++)
+            {
+                far[i] += "a";
+                Console.WriteLine(far[i]);
+            }*/
+
+            foreach (string animals in far)
+            {
+                var fixedAnimals = new StringBuilder();
+                fixedAnimals.Append(animals);
+                fixedAnimals.Append("a");
+                Console.WriteLine(fixedAnimals);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
