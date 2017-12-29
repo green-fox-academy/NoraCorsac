92b56321d4ef687c996c0c592e758417e04196a5
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 15:09:46 +0100

    feat(SumAllElement): add for loop

---
+++ after-exam-practice/array/SumAllElement/SumAllElement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumAllElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+            int sum = 0;
+
+            for (int i = 0; i < ai.Length; i++)
+            {
+                sum = sum + ai[i];
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
