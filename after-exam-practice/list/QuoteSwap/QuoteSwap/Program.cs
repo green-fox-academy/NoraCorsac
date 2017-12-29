81b720a2aa9aed30523c9a0f4b7baf443974b66e
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 18:01:34 +0100

    feat(QuoteSwap): swap words

---
+++ after-exam-practice/list/QuoteSwap/QuoteSwap/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuoteSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            list[2] = "cannot";
+            list[5] = "do";
+
+            foreach (string quote in list)
+            {
+                Console.Write(quote + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
