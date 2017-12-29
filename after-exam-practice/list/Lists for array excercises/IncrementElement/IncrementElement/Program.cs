4ab3e6d868e0044fef9df8c09a76b733f1ad1f4d
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 22:56:52 +0100

    feat(IncrementElement): convert array to list

---
+++ after-exam-practice/list/Lists for array excercises/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 1, 2, 3, 4, 5 };
+            List<int> tList = new List<int>();
+            tList = t.ToList();
+            Console.WriteLine(tList[2] + 2);
+            Console.ReadLine();
+        }
+    }
+}
