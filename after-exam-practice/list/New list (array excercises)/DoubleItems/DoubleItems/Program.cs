9efbf7cd3a229285c80b47fdb9a76c0455bced7e
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 23:23:32 +0100

    feat(DoubleItems): add new list

---
+++ after-exam-practice/list/New list (array excercises)/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ag = new List<int> { 3, 4, 5, 6, 7 };
+            foreach (int number in ag)
+            {
+                Console.WriteLine(number + " " + number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
