f220810bcf1689ec9ba89d23783ba67e997fe441
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 16:30:12 +0100

    feat(SolarSystem): add a planet

---
+++ after-exam-practice/list/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+
+            planetList.Insert(5, "Saturn");
+            for (int i = 0; i < planetList.Count; i++)
+            {
+                Console.WriteLine(planetList[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
