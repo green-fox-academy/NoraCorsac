33fab9cef14a238ef5f1d6212bbe475ee240d086
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 14:47:13 +0100

    feat(Colors): add colors

---
+++ after-exam-practice/array/Colors/Colors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] colors =
+            {
+                { "lime", "forest green", "olive", "pale green", "spring green" },
+                { "orange red", "red", "tomato", "", "" },
+                { "orchid", "violet", "pink", "hot pink", "" }
+            };
+
+            Console.WriteLine(colors[2, 1]);
+
+            Console.ReadLine();
+        }
+    }
+}
