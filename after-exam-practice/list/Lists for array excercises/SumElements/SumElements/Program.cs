f6dea7288d433dee1cc78aeee2cf5013a328307c
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 22:44:57 +0100

    feat(SumElements): convert array to list

---
+++ after-exam-practice/list/Lists for array excercises/SumElements/SumElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = { 54, 23, 66, 12 };
+            List<int> rList = new List<int>();
+            rList = r.ToList();
+            Console.WriteLine(rList[1] + rList[2]);
+            Console.ReadLine();
+        }
+    }
+}
