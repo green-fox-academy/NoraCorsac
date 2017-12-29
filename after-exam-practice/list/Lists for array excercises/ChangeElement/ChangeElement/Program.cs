62708ad4cc91016eee1554e432e2c795c74b234f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 22:48:41 +0100

    feat(ChangeElement): convert array to list

---
+++ after-exam-practice/list/Lists for array excercises/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            List<int> sList = new List<int>();
+            sList = s.ToList();
+            sList[3] = 4;
+
+            Console.WriteLine(sList [3]);
+            Console.ReadLine();
+        }
+    }
+}
