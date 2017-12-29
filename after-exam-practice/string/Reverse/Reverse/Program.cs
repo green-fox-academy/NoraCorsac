1762a5be7fe8ddd5bbdd2aa765bed2ba7f5a8aa1
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 16:21:14 +0100

    feat(Reverse): change type to reverse

---
+++ after-exam-practice/string/Reverse/Reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            char[] inputreversed = reversed.ToCharArray();
+            Array.Reverse(inputreversed);
+            string reversed2 = new string(inputreversed);
+
+            Console.WriteLine(reversed2);
+            Console.ReadLine();
+        }
+    }
+}
