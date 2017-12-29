06890b63529b69e0f64a4406287ccc353b40dcca
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 15:15:22 +0100

    feat(Reverse): add more solutions

---
+++ after-exam-practice/array/Reverse/Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+
+            foreach (int element in aj)
+            {
+                Console.WriteLine(element);
+            }
+
+            /*for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(aj[i]);
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
