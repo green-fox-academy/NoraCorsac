e9a11bb32e4fe3cd386b51c4d3d71c1d0f88b976
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 15:04:17 +0100

    feat(SwapElement): add more solutions

---
+++ after-exam-practice/array/SwapElement/SwapElement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+
+            //Console.WriteLine(abc[2] + "\n" +  abc[1] + "\n" + abc[0]);
+
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            Console.WriteLine(abc[0] + "\n" + abc[1] + "\n" + abc[2]);
+
+            /*for (int i = 2; i < abc.Length; i--)
+            {
+                Console.WriteLine(abc[i]);
+            }*/
+
+            Console.ReadLine();
+        }
+    }
+}
