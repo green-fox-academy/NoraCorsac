780c84dfb0f68fd7bd27edf54c634dd2853c5577
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 22:31:12 +0100

    feat(Third): convert array to list

---
+++ after-exam-practice/list/Lists for array excercises/Third/Third/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = { 4, 5, 6, 7 };
+            List<int> qList = new List<int>();
+            qList = q.ToList();
+
+            Console.WriteLine(qList[2]);
+            Console.ReadLine();
+        }
+    }
+}
