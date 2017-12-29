c910b816a483538e52ce56c8ad0d123612a36112
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-29 06:10:37 +0100

    feat(CompareLength): add if-else statement

---
+++ after-exam-practice/array/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p1 = { 1, 2, 3 };
+            int[] p2 = { 4, 5, };
+
+            if (p1.Length < p2.Length)
+            {
+                Console.WriteLine("p2 has more elements, than p1.");
+            }
+            else
+            {
+                Console.WriteLine("Ooops!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
