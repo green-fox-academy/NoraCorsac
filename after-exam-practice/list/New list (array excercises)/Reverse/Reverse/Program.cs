b4588ca15523f05a62bebb0dcdfe3bb2e10744ad
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-30 12:15:57 +0100

    feat(Reverse): add .Reverse

---
+++ after-exam-practice/list/New list (array excercises)/Reverse/Reverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> aj = new List<int> { 3, 4, 5, 6, 7 };
+            aj.Reverse();
+            foreach (int number in aj)
+            {
+            Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
