87a6a4ef2fefe1eb0d2501ed9709bd171b92f63f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-30 11:57:56 +0100

    feat(SwapElements): swap the elements

---
+++ after-exam-practice/list/New list (array excercises)/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> abc = new List<string> { "first", "second", "third" };
+            abc[2] = abc[0];
+            abc[0] = "third";
+            foreach (string element in abc)
+            {
+            Console.WriteLine(element);
+            }
+            Console.ReadLine();
+        }
+    }
+}
