8586c658ce5cffff08230aec12d17de57b6dc395
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-30 12:06:54 +0100

    feat(SumAll): use .sum

---
+++ after-exam-practice/list/New list (array excercises)/SumAll/SumAll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ai = new List<int> { 3, 4, 5, 6, 7 };
+
+            int sum = ai.Sum();
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
