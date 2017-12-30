d2aadcb310b16a37c9247c5af9eaaa1dfd673df8
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-30 14:56:37 +0100

    feat(DivideByZero): add try-catch-finally

---
+++ after-exam-practice/file io/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type in the divisor number: ");
+
+            try
+            {
+                double divisorNumber = double.Parse(Console.ReadLine());
+                double result = 10 / divisorNumber;
+                Console.WriteLine("10 / " + divisorNumber + " = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("fail");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
