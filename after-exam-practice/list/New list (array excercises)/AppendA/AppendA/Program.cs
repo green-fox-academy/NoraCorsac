4c958c928a8f26a2e1c4d17ecca4fc523b5cdf6f
Nora Fraknoy <norafraknoy@gmail.com>
2017-12-30 11:47:24 +0100

    feat(AppendA): add a letter with foreach

---
+++ after-exam-practice/list/New list (array excercises)/AppendA/AppendA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nimals = new List<string> { "kuty", "macsk", "cic" };
+            foreach (string animal in nimals)
+            {
+                Console.WriteLine(animal + "a");
+            }
+            Console.ReadLine();
+        }
+    }
+}
