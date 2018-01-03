e3c12218413d79420c1791c34618dac8153d4ba5
Nora Fraknoy <norafraknoy@gmail.com>
2018-01-03 15:21:04 +0100

    feat(SquaredValue): positive numbers on sqare

---
+++ week-06/Day-3/SquaredValue/SquaredValue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaredValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var squaredPositiveNumbers = from i in n
+                                         where i > 0
+                                         select i;
+            foreach (var number in squaredPositiveNumbers)
+            {
+                Console.WriteLine(number * number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
