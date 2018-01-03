1197c4db874334903152227d2d0c60212e0331b4
Nora Fraknoy <norafraknoy@gmail.com>
2018-01-03 14:59:37 +0100

    feat(Average): calc average

---
+++ week-06/Day-3/Average/Average/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            double average = n.Average();
+            Console.WriteLine(average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-06/Day-3/EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var evenNumbers = from i in n
+                              where i % 2 == 0
+                              select i;
+            foreach (var numbers in evenNumbers)
+            {
+                Console.WriteLine(numbers);
+            }
+            Console.ReadLine();
+        }
+    }
+}
