0cd8c04507e61b021d310f945a50862dce6e3c45
Nora Fraknoy <norafraknoy@gmail.com>
2018-01-17 17:21:47 +0100

    feat(GreetSomeone): greets someone

---
+++ week-07/day-4/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        Greet Greeting;
+
+        public HomeController(Greet greet)
+        {
+            Greeting = greet;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Addname(Greet greet)
+        {
+            Greeting.Name = greet.Name;
+            return RedirectToAction("Hello");
+        }
+
+        [HttpPost("Hello")]
+        public IActionResult Hello(Greet G)
+        {
+            Greeting.Name = G.Name;
+            return View(G);
+        }        
+    }
+}
---
+++ week-07/day-4/GreetSomeone/GreetSomeone/Models/Greet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Models
+{
+    public class Greet
+    {
+        public string Name { get; set; }
+
+        public Greet(string name)
+        {
+            Name = name;
+        }
+
+        public Greet()
+        {
+        }
+
+        public string SayHello()
+        {
+            return "Hello, " + Name;
+        }
+    }
+}
