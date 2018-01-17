f891b0acfa03fb2833381679b597d894a1c0fbf5
Nora Fraknoy <norafraknoy@gmail.com>
2018-01-17 16:10:18 +0100

    feat(Counter2): finish

---
+++ week-07/day-4/Counter2/Counter2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Counter2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Counter2.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private CounterModel counter;
+        public HomeController(CounterModel counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost("")]
+        public IActionResult Add()
+        {
+            counter.Number += 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-07/day-4/Counter2/Counter2/Models/CounterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter2.Models
+{
+    public class CounterModel
+    {
+        public int Number { get; set; }
+
+        public CounterModel()
+        {
+            Number = 0;
+        }
+    }
+}
