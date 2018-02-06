40df82885bf8514819061763ae8f3ceecbc2692c
Nora Fraknoy <norafraknoy@gmail.com>
2018-02-06 13:27:52 +0100

    feat(Anagram): DI

---
+++ week-07/day-4/Anagram/Anagram/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Anagram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anagramm.Cotrollers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        Anagramma anagramms;
+
+        public HomeController(Anagramma anagramms)
+        {
+            this.anagramms = anagramms;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult Checker(Anagramma anagrammas)
+        {
+            anagramms.Word1 = anagrammas.Word1;
+            anagramms.Word2 = anagrammas.Word2;
+
+            return RedirectToAction("Result");
+        }
+
+        [HttpGet("Result")]
+        public IActionResult Result()
+        {
+            return View(anagramms);
+        }
+    }
+}
---
+++ week-07/day-4/Anagram/Anagram/Models/Anagramma.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagram.Models
+{
+    public class Anagramma
+    {
+        public string Word1 { get; set; }
+        public string Word2 { get; set; }
+
+        public Anagramma()
+        {
+        }
+
+        public bool AnagrammChecker()
+        {
+            bool IsItAnagramm = true;
+
+            char[] firstWord = Word1.ToCharArray().OrderBy(x => x).ToArray();
+            char[] secondWord = Word2.ToCharArray().OrderBy(x => x).ToArray();
+
+            if (firstWord.Length == secondWord.Length)
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    if (firstWord[i] != secondWord[i])
+                    {
+                        IsItAnagramm = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                IsItAnagramm = false;
+            }
+            return IsItAnagramm;
+        }
+    }
+}
